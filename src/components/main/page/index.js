cd19a48a8d5c9c460151b36e92a7ddb2270416e7
Sehbeom <j3beom@gmail.com>
2022-05-19 06:24:21 +0900

    :sparkles: styled-component 추가

---
+++ src/components/main/page/index.js
@@ -0,0 +1,13 @@
+import * as S from './style';
+
+function MainPage() {
+  return (
+    <S.Container>
+        <div>
+            <S.Text>MainPage</S.Text>
+        </div>
+    </S.Container>
+  );
+}
+
+export default MainPage;
